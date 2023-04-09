4de229a29834ca770571f86e26c0a834aac29cec
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2023-04-09 11:40:58 +0300

    initial structure

---
+++ src/log.py
@@ -0,0 +1,14 @@
+class Colors:
+    OK = '\033[92m'
+    INFO = '\033[94m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+
+def print_info(text: str) -> None:
+    print(f'{Colors.INFO}{text}{Colors.END}')
+
+def print_ok(text: str) -> None:
+    print(f'{Colors.OK}{text}{Colors.END}')
+
+def print_fail(text: str) -> None:
+    print(f'{Colors.FAIL}{text}{Colors.END}')
